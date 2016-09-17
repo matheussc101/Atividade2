451ad04d8a966867aae4229a3856c3335604e9e4
alunoinf <alunoinf@192.168.40.191>
2016-09-16 21:48:13 -0300

    Adicionando Controller

---
+++ atividades/src/main/java/atividades/GestaoAcademicaController.java
@@ -0,0 +1,22 @@
+package atividades;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/index")
+public class GestaoAcademicaController extends HttpServlet {
+	
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+	}
+	
+
+}
+	
